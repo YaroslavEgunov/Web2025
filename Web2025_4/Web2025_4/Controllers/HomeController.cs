890557e33592846bb93570f31ee6803970a7d611
Egunov Yaroslav <antonioshmonderos@gmail.com>
2024-12-05 00:27:14 +0700

    Готово, вроде

---
+++ Web2025_4/Web2025_4/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web2025_4.Models;
+
+namespace Web2025_4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Task1()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
